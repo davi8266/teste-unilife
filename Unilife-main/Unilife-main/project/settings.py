e714c4bdc5de24414cb09be8c4dff0fdd154a4f3
davi <126797775+davi8266@users.noreply.github.com>
2025-08-30 16:37:12 -0300

    Update settings.py

--- Unilife-main/Unilife-main/project/settings.py
+++ Unilife-main/Unilife-main/project/settings.py
@@ -22,9 +22,6 @@ CSRF_TRUSTED_ORIGINS = ["https://*.trycloudflare.com"]
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-
 # Application definition
 
 INSTALLED_APPS = [
