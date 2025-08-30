740d87f9637147ae4043e603f1d9f3a7ab98d21e
davi <126797775+davi8266@users.noreply.github.com>
2025-08-30 16:36:17 -0300

    Update settings.py

--- Unilife-main/Unilife-main/project/settings.py
+++ Unilife-main/Unilife-main/project/settings.py
@@ -13,11 +13,17 @@ SECRET_KEY = 'django-insecure-ylw)ixz8wy6uinylae4*bsma@e9x(*4(s$1vnw_@qsmi)113g9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.1.156", "localhost", "127.0.0.1", "raspberrypi.local", ".trycloudflare.com", "*"]
+ALLOWED_HOSTS = [
+    "192.168.1.156", "localhost", "127.0.0.1", "raspberrypi.local",
+    ".trycloudflare.com"
+]
 
 CSRF_TRUSTED_ORIGINS = ["https://*.trycloudflare.com"]
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
