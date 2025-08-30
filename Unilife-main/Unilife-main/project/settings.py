f2ed9cbc82f0d066e2bdece845ce033251fc9e8c
davi <126797775+davi8266@users.noreply.github.com>
2025-08-30 19:04:34 -0300

    Update settings.py

--- Unilife-main/Unilife-main/project/settings.py
+++ Unilife-main/Unilife-main/project/settings.py
@@ -14,7 +14,13 @@ SECRET_KEY = 'django-insecure-ylw)ixz8wy6uinylae4*bsma@e9x(*4(s$1vnw_@qsmi)113g9
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "192.168.1.156", "localhost", "127.0.0.1", "raspberrypi.local",".trycloudflare.com", ]
+    "192.168.1.156", "localhost", "127.0.0.1", "raspberrypi.local",
+    ".trycloudflare.com",
+]
+
+
+CSRF_TRUSTED_ORIGINS = ["https://*.trycloudflare.com"]
+
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
